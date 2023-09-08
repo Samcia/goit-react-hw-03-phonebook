a7f628bc7941b147948d08274159fc04412a8aa0
Samcia <samantta689@gmail.com>
2023-09-08 15:32:43 +0200

    update App saving data in LS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,16 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+  constructor() {
+    super();
+    const savedContacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(savedContacts);
+    this.state.contacts = parsedContacts || [];
+  }
+
   addContact = event => {
     event.preventDefault();
 
@@ -48,6 +50,10 @@ class App extends Component {
       contacts: prevState.contacts.filter(contact => contact.id !== id),
     }));
   };
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
   render() {
     const { contacts, filter } = this.state;
     return (
--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -14,7 +14,7 @@ export const ContactsForm = ({ addContact }) => {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          placeholder="Write your name"
+          placeholder="Your name"
         />
       </label>
       <label>
@@ -26,7 +26,7 @@ export const ContactsForm = ({ addContact }) => {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
-          placeholder="Write your number"
+          placeholder="Your number"
         />
       </label>
       <button className="form__button" type="submit">
